8dd5e26318cce5effd30587911c8dd9de24910e4
unikZer0 <unik09john@gmail.com>
2025-04-01 16:11:56 +0700

    ip

--- backend/src/setting/connection.js
+++ backend/src/setting/connection.js
@@ -1,6 +1,6 @@
 const mysql2 = require('mysql2/promise');
 
-const host = '100.91.194.104'|| '100.75.106.34'
+const host =  '100.75.106.34'
 const user = 'Admin'
 const password = 'admin123'
 const database = 'tuv_test'
